f43ea8e7b180aead2bab8faebc20e86b59dea138
Valentin <v.boss@novum-engineering.biz>
2016-10-30 00:22:45 +0200

    Added config_parser module to piwordclock package.

---
+++ piwordclock/config_parser.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Valentin'
+
+import json
+
+
+def read_config_file(path_to_file):
+    """Reads the config file in json format and returns it as a dictionary."""
+    with open(path_to_file) as json_raw_data:
+        config_data = json.load(json_raw_data)
+
+    json_raw_data.close()
+    return config_data
+
+
+def get_wordclock_version(json_data_dict):
+    """Returns the WordClock Version."""
+    return json_data_dict["WordClock Version"]
+
+
+def get_wordclock_binary_extension(json_data_dict):
+    """Returns if the WordClock has the binary extension set."""
+    return json_data_dict["Binary Extension"]
+
+
+def get_wordclock_start_up_on_color(json_data_dict):
+    """Returns the start up on color of the WordClock as a dictionary."""
+    return json_data_dict["Start Up On Color"]
+
+
+def get_wordclock_start_up_off_color(json_data_dict):
+    """Returns the start up off color of the WordClock as a dictionary."""
+    return json_data_dict["Start Up Off Color"]
+
+
+def print_configuration(config_data):
+    """Prints the current start up configuration of the WordClock."""
+    print("WordClock Version: " + config_data["WordClock Version"])
+    print("Binary Extension: " + str(config_data["Binary Extension"]))
+    start_up_colors_on = config_data["Start Up On Color"]
+    print("Start Up Color On Values: " + "R: " + str(start_up_colors_on["red"]) + " G: " +
+          str(start_up_colors_on["green"]) + " B: " + str(start_up_colors_on["blue"]))
+    start_up_colors_off = config_data["Start Up Off Color"]
+    print("Start Up Color Off Values: " + "R: " + str(start_up_colors_off["red"]) + " G: " +
+          str(start_up_colors_off["green"]) + " B: " + str(start_up_colors_off["blue"]))
