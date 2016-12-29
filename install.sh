7f5221e86978d63d72704d57ad9284c7e7fbc9d9
valeboss <v.boss@gmx.de>
2016-12-29 22:24:02 +0100

    Minor changes and bugfixes in the installation shell script.

--- install.sh
+++ install.sh
@@ -2,17 +2,16 @@
 
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install git scons swig build-essential python3 python3-dev
-git clone https://github.com/jgarff/rpi_ws281x
+mkdir -r Programme
+git clone https://github.com/jgarff/rpi_ws281x ~/Programme/
 cd rpi_ws281x
 scons
 cd python
 sudo python3 setup.py install
-cd ../
-git clone https://github.com/tornadoweb/tornado.git
-cd tornado
-python setup.py build
+git clone https://github.com/tornadoweb/tornado.git ~/Programme/
+cd ~/Programe/tornado
+python3 setup.py build
 sudo python3 setup.py install
-cd ../
-git clone https://github.com/valeboss/piWordClock.git
-cd piWordClock
+git clone https://github.com/valeboss/piWordClock.git ~/Programme/
+cd ~/Programme/piWordClock
 sudo python3 piwordclock.py
